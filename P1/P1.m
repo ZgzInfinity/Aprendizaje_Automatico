1c7f26cfa82a0f0f4fec5cbdcb250732e4f2dd60
ZgzInfinity <38316325+ZgzInfinity@users.noreply.github.com>
2019-03-11 17:52:10 +0100

    Practica 1 ok

--- P1/P1.m
+++ P1/P1.m
@@ -20,50 +20,57 @@ N2 = length(y2);
 
 %APARTADO 2
 
-function ecNormalMono(V, S, D, t)
+function ecNormalMono(VTrain, VTest, STr, STest, DTrain, DTest)
   %% Dibujo de un Ajuste Monovariable test
   figure;
-  plot(V, S, 'bx');
+  plot(VTrain, STr, 'bx');
   title('Precio de los Pisos')
   ylabel('Euros'); xlabel('Superficie (m^2)');
   grid on; hold on; 
 
-  X = [ones(D,1) V];
-  th = X \ S;  % Pongo un valor cualquiera de pesos
-  Xextr = [1 min(V)  % Predicción para los valores extremos
-         1 max(V)];
+  X = [ones(DTrain,1) VTrain];
+  th = X \ STr;  % Pongo un valor cualquiera de pesos
+  Xextr = [1 min(VTrain)  % Predicción para los valores extremos
+         1 max(VTrain)];
   yextr = Xextr * th;  
   plot(Xextr(:,2), yextr, 'r-'); % Dibujo la recta de predicción
-  if (t == 0) 
-    legend('Datos Entrenamiento', 'Ecuacion normal')
-  else
-    legend('Datos Test', 'Ecuacion normal')
-  end
+  legend('Datos Entrenamiento', 'Ecuacion normal')
+
+  fprintf('Calculo de la prediccion\n\n')
+  th
   
-  'Prediccion dd 100 m2'
-  x100 = sum (th .* [1 100]')
+  r = X * th - STr;
+  medR = mean(abs(r));
+
+  fprintf('Error medio de los datos de entrenamiento %d \n\n', medR)
+  
+  X = [ones(DTest,1) VTest];
+  r = X * th - STest;
+  medR = mean(abs(r));
+
+  fprintf('Error medio de los datos de test %d \n\n', medR)
   
 end
 
 % APARTADO 3
 
-function ecNormalBiDos(V1, V2, S, D)
+function ecNormalBiDos(V1Train, V2Train, V1Test, V2Test, STrain, STest, DTrain, DTest)
   %% APARTADO 3
   %% Entrenamiento
-  X = [ones(D,1) V1 V2];
-  th = X \ S;  % Pongo un valor cualquiera de pesos
+  X = [ones(DTrain,1) V1Train V2Train];
+  th = X \ STrain;  % Pongo un valor cualquiera de pesos
   yest = X * th;
 
   % Dibujar los puntos de entrenamiento y su valor estimado 
   figure;  
-  plot3(V1, V2, S, '.r', 'markersize', 20);
+  plot3(V1Train, V2Train, STrain, '.r', 'markersize', 20);
   axis vis3d; hold on;
-  plot3([V1 V1]' , [V2 V2]' , [S yest]', '-b');
+  plot3([V1Train V1Train]' , [V2Train V2Train]' , [STrain yest]', '-b');
 
   % Generar una retícula de np x np puntos para dibujar la superficie
   np = 20;
-  ejex1 = linspace(min(V1), max(V1), np)';
-  ejex2 = linspace(min(V2), max(V2), np)';
+  ejex1 = linspace(min(V1Train), max(V1Train), np)';
+  ejex2 = linspace(min(V2Train), max(V2Train), np)';
   [x1g,x2g] = meshgrid(ejex1, ejex2);
   x1g = x1g(:); %Los pasa a vectores verticales
   x2g = x2g(:);
@@ -80,18 +87,33 @@ function ecNormalBiDos(V1, V2, S, D)
   % estimacion de las predicciones
   % baja conforme aumentan las habitaciones
   
-  'Prediccion 100 m2 y 2 dormitorios'
-  x1002 = sum (th .* [1 100 2]')
-
-  'Prediccion 100 m2 y 3 dormitorios'
-  x1003 = sum (th .* [1 100 3]')
-
-  'Prediccion 100 m2 y 4 dormitorios'
-  x1004 = sum (th .* [1 100 4]')
-
-  'Prediccion 100 m2 y 5 dormitorios'
-  x1005 = sum (th .* [1 100 5]')
+  fprintf('Calculo de la prediccion\n');
+  th
+  
+  r = X * th - STrain;
+  medR = mean(abs(r));
+  
+  fprintf('Error de entrenamiento %d\n', medR)
+  
+  x1002 = sum (th .* [1 100 2]');
+  fprintf('Prediccion 100 m2 y 2 dormitorios %d\n', x1002) 
+  
+  x1003 = sum (th .* [1 100 3]');
+  fprintf('Prediccion 100 m2 y 3 dormitorios %d\n', x1003) 
+  
+  x1004 = sum (th .* [1 100 4]');
+  fprintf('Prediccion 100 m2 y 4 dormitorios %d\n', x1004) 
+  
+  x1005 = sum (th .* [1 100 5]');
+  fprintf('Prediccion 100 m2 y 5 dormitorios %d\n', x1005) 
+  
+  X = [ones(DTest,1) V1Test V2Test];
+  r = X * th - STest;
+  medR = mean(abs(r));
+  
+  fprintf('Valor del error del test %d\n\n', medR)
 end
+
 
 % APARTADO 4
 
@@ -104,6 +126,7 @@ function [J, grad] = CosteHuber(theta,X,y, d, r)
   end
  end
 
+ 
 % Coste cuadratico
 function [J , grad] = CosteL2(theta,X,y,r) 
   % Calcula el coste cuadrático
@@ -113,18 +136,19 @@ function [J , grad] = CosteL2(theta,X,y,r)
   end
 end
 
+
 % funcion iterativa del calculo de descenso de gradiente
-function [th, i] = iterar (X, th, y, t, C, I)
+function [th, i] = iterar (X, th, yTrain, t, C, I, DTrain, DTest)
     alfa = 0.001;
     fi = 50000;
-    r = X * th - y;
+    r = X * th - yTrain;
     % determinar que coste usar
     if (t == 0)  
       % cuadratico
-      [nC, g] = CosteL2(th, X, y, r);
+      [nC, g] = CosteL2(th, X, yTrain, r);
     else
       % Huber
-      [nC, g] = CosteHuber(th, X, y, fi, r); 
+      [nC, g] = CosteHuber(th, X, yTrain, fi, r); 
     end
     % Incorporar nuevo coste
     C = [C nC];    
@@ -134,14 +158,14 @@ function [th, i] = iterar (X, th, y, t, C, I)
     do 
       i = i + 1;
       th = th - alfa * g;
-      r = X * th - y;
+      r = X * th - yTrain;
       % determinar que coste usar
       if (t == 0)  
         % cuadratico
-        [nC, g] = CosteL2(th, X, y, r);
+        [nC, g] = CosteL2(th, X, yTrain, r);
       else
         % Huber
-        [nC, g] = CosteHuber(th, X, y, fi, r); 
+        [nC, g] = CosteHuber(th, X, yTrain, fi, r); 
       end
       % Incorporar nuevo coste
       C = [C nC];    
@@ -149,8 +173,13 @@ function [th, i] = iterar (X, th, y, t, C, I)
       I = [I i];
     until ((abs(C(i) - C(i-1))) < 0.001)
     
+    fprintf('Calculo de prediccion con descenso de gradiente\n');
+    
     %Calculo de las media
-    medR = mean(abs(r))
+    medR = mean(abs(r));
+    
+    fprintf('Numero total de iteraciones: %d\n', i)
+    fprintf('Error medio de los datos de entrenamiento %d\n\n', medR)
     
     % regresion efectuada
     figure;
@@ -162,30 +191,44 @@ function [th, i] = iterar (X, th, y, t, C, I)
       title('Coste de Huber')
       ylabel('J(theta'); xlabel('Iteracion');
     end
+    
 end
 
 
-function [th] = descensoGradiente(X, th, y, t) 
+function [th] = descensoGradiente(XTrain, XTest, th, yTrain, ySet, t, N1, N2) 
   % falta hallar th inicial
   % Ejemplo funcion de regresión
-  %Normalizar los atributos 
-  N = size(X,1); 
-  mu = mean(X(:,2:end)); 
-  sig = std(X(:,2:end)); 
-  X(:,2:end) = (X(:,2:end) - repmat(mu,N,1))./ repmat(sig,N,1);
+  %Normalizar los atributos entrenamiento
+  NTrain = size(XTrain,1); 
+  muTrain = mean(XTrain(:,2:end)); 
+  sigTrain = std(XTrain(:,2:end)); 
+  XTrain(:,2:end) = (XTrain(:,2:end) - repmat(muTrain,NTrain,1))./ repmat(sigTrain,NTrain,1);
+  
+  % falta hallar th inicial
+  % Ejemplo funcion de regresión
+  %Normalizar los atributos test
+  NTest = size(XTest,1); 
+  muTest = mean(XTest(:,2:end)); 
+  sigTest = std(XTest(:,2:end)); 
+  XTest(:,2:end) = (XTest(:,2:end) - repmat(muTest,NTest,1))./ repmat(sigTest,NTest,1);
+  
   % Vector de costes e iteraciones vacios
   C = [];
   I = [];
   % Decision del valor de tasa de aprendizaje alfa
   %Resolver la Regresión
-  [th,i] = iterar( X, th', y, t, C, I);
+  [th,i] = iterar( XTrain, th', yTrain, t, C, I , N1, N2);
+  
+  %Calculo del error en test
+  r = XTest * th - ySet;
+  medR = mean(abs(r));
+  
+  fprintf('Error de test con descenso de Gradiente %d\n\n', medR)
     
   %Des-Normalizarlos pesos 
-  th(2:end) = th(2:end)./sig'; 
-  th(1)= th(1)-(mu*th(2:end));
+  th(2:end) = th(2:end)./sigTrain'; 
+  th(1)= th(1)-(muTrain*th(2:end));
   
-  %th
-  %i
 end 
 
 
@@ -193,99 +236,55 @@ end
  
  %APARTADO 2
  
+ fprintf(' ---------- Ejercicio 2 ------------------- \n\n')
+ 
  % regresion monovariable con datos de entrenamiento
  % ecuacion normal
- ecNormalMono(x1, y1, N1, 0)
+ ecNormalMono(x1, x3, y1, y2, N1, N2)
  
- % regresion monovariable con datos de test
- % ecuacion normal
- ecNormalMono(x3, y2, N2, 1)
+ 
  
  % APARTADO 3
  
- % regresion multivariable con datos de entrenamiento
- % ecuacion normal
- ecNormalBiDos(x1, x2, y1, N1)
+ fprintf(' ---------- Ejercicio 3 ---------------- \n\n')
  
  % regresion multivariable con datos de entrenamiento
  % ecuacion normal
- ecNormalBiDos(x3, x4, y2, N2)
+ ecNormalBiDos(x1, x2, x3, x4, y1, y2, N1, N2)
  
+
  % APARTADO 4
+ 
+ fprintf(' ---------- Ejercicio 4 ---------------- \n\n')
  
  % regresion monovariable con datos de entrenamiento
  % descenso de gradiente
- M = [ones(N1,1) x1];
- descensoGradiente(M, [5000 1000], y1, 0)
+ MTrain = [ones(N1,1) x1];
+ MTest = [ones(N2,1) x3];
+ descensoGradiente(MTrain, MTest, [5000 1000], y1, y2, 0 , N1, N2)
  
- %th =
- %-3.4313e+004
- %2.6082e+003
- %i =  58
- %medR =   6.9535e+004
- 
- % regresion monovariable con datos de test
- % descenso de gradiente
- M = [ones(N2,1) x3];
- descensoGradiente(M, [5000 1000], y2,0)
- 
- %th =
- %1.4936e+004
- %2.0086e+003
- %i =  525
- %medR =   6.0287e+004
- 
+
  %APARTADO 5
  
+ fprintf(' ---------- Ejercicio 5 ---------------- \n\n')
+ 
  % regresion multivariable con datos de entrenamiento
  % descenso de gradiente
- M = [ones(N1,1) x1 x2];
- descensoGradiente(M,[5000 1000 50000], y1, 0)
+ MTrain = [ones(N1,1) x1 x2];
+ MTest = [ones(N2,1) x3 x4];
+ descensoGradiente(MTrain, MTest, [5000 1000 50000], y1, y2, 0, N1, N2)
  
- %th =
- %-1.2133e+004
- %3.0287e+003
- %-1.8853e+004
- %i =  233
- %medR =   6.9089e+004
- 
- % regresion multivariable con datos de test
- % descenso de gradiente
- M = [ones(N2,1) x3 x4];
- descensoGradiente(M, [5000 1000 50000], y2, 0)
- 
- 
- %th =
- %1.1571e+005
- %2.6093e+003
- %-4.5301e+004
- %i =  963
- %medR =   4.4044e+004
- 
- 
+
  % APARTADO 6 OPCIONAL
  
+ fprintf(' ---------- Ejercicio 6 ---------------- \n\n')
+ 
  % regresion multivariable con datos de entrenamiento
  % coste Huber
- M = [ones(N1,1) x1 x2];
- descensoGradiente(M,[5000 1000 50000], y1, 1)
+ MTrain = [ones(N1,1) x1 x2];
+ MTest = [ones(N2,1) x3 x4];
+ descensoGradiente(MTrain, MTest, [5000 1000 50000], y1, y2, 1, N1 , N2)
  
- %th =
- %-1.2133e+004
- %3.0287e+003
- %-1.8853e+004
- %i =  413
- %medR =   6.7718e+004
- 
- % regresion multivariable con datos de test
- % coste Huber
- M = [ones(N2,1) x3 x4];
- descensoGradiente(M, [5000 1000 50000], y2, 1)
- 
- %th =
- %1.1571e+005
- %2.6093e+003
- %-4.5301e+004
- %i =  1628
- %medR =   4.2394e+004
+
+
  
