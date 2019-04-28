4f6d32e3900659839383b1989d98f225b8fc6988
DESKTOP-6V36O5F\Gord <737215@unizar.es>
2019-04-28 17:26:46 +0200

    ej2 ahora con sentido

--- P6/labP6-PCA/P62.m
+++ P6/labP6-PCA/P62.m
@@ -30,28 +30,63 @@ mCov = cov(Xest);
 % Calculo de las matrices U, S, V con svd
 [U,S,V] = svd(mCov);
 
-% Calculo del valor de la matriz Z con en las nuevas dimensiones
-Z = U' * Xest';
+% Ordenar la matriz de valores propios de mayor a menor
+sDiagAscend = diag(sort(diag(S)','descend'));
+sDiag = sort(diag(S)','descend');
+sDiagRef = diag(S)';
 
-% Calculo del valor de Xhat 
-Xgorro = U * Z;
-Xhat = Xgorro';
+
+% La matriz de los vectores propios ordenada de mayor a menor
+% segun su valor propio es la siguiente
+[~, I] = sort(sDiagRef, 'descend');
+Uord = U(:,I');
+
+
+%Identificador de imagen
+idImagen = 1;
 
 % Graficar las primeras 5 componentes
 for k = 1:5,
-    figure(2);
+    figure(idImagen);
+    
+    % Coger la K primeras columnas de la matriz U ordenada
+    Ureduce = Uord(1:size(Uord,1),1:k);
+    
+    % Obtencion de la matriz Z para K dimensiones
+    Z = Ureduce' * Xest';
+    Zprim = Z';
+    
+    % Reconstruccion de los datos para k dimensiones a partir de Z
+    Xgorro = Ureduce * Z;
+    Xhat = Xgorro';
     imshow(Xhat);
     colormap(gray);
     axis off;
+    
+    %Incremento del id de la imagen
+    idImagen = idImagen + 1;
 end
 
 % Graficar la reconstrucción con las primeras 1, 2, 5, 10, 20, y total
 % de componentes
 for k = [1 2 5 10 20 rank(X)],
-    figure(3);
+    figure(idImagen);
+    % Coger la K primeras columnas de la matriz U ordenada
+    Ureduce = Uord(1:size(Uord,1),1:k);
+    
+    % Obtencion de la matriz Z para K dimensiones
+    Z = Ureduce' * Xest';
+    Zprim = Z';
+    
+    % Reconstruccion de los datos para k dimensiones a partir de Z
+    Xgorro = Ureduce * Z;
+    Xhat = Xgorro';
     imshow(Xhat);
     colormap(gray);
     axis off;
+    
+    %Incremento del id de la imagen
+    idImagen = idImagen + 1;
 end
 
 
@@ -61,12 +96,26 @@ end
 fprintf('La suma primera suma que ha sido superior a 0.90 ha sido %f\n', sumaK);
 fprintf('El valor de k es el siguiente: %d\n', k);
 
-% Graficar la reconsrtucción con las primeras k componentes
+% Graficar la reconstrucción con las primeras k componentes
 for i = 1:k,
-    figure(4);
+    figure(idImagen);
+    
+    % Coger la K primeras columnas de la matriz U ordenada
+    Ureduce = Uord(1:size(Uord,1),1:k);
+    
+    % Obtencion de la matriz Z para K dimensiones
+    Z = Ureduce' * Xest';
+    Zprim = Z';
+    
+    % Reconstruccion de los datos para k dimensiones a partir de Z
+    Xgorro = Ureduce * Z;
+    Xhat = Xgorro';
     imshow(Xhat);
     colormap(gray);
     axis off;
+    
+    %Incremento del id de la imagen
+    idImagen = idImagen + 1;
 end
 
 % Calcular y mostrar el ahorro en espacio
