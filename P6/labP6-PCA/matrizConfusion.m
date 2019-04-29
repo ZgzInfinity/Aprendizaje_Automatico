cd82d9278cae7dfb69d7a4e2873dc5643231df24
DESKTOP-6V36O5F\Gord <737215@unizar.es>
2019-04-29 23:48:05 +0200

    añadido F_score

--- P6/labP6-PCA/matrizConfusion.m
+++ P6/labP6-PCA/matrizConfusion.m
@@ -1,5 +1,8 @@
 function [ precision, recall] = matrizConfusion( p, y, n1 , n2)
 
+  % Vector de cocientes F_score
+  F1_ScoresMat = [];
+
   for clase = [ n1 , n2 ]
       %TN - True negative
       tn = (sum(double((p~=clase)&(y~=clase))));
@@ -15,9 +18,23 @@ function [ precision, recall] = matrizConfusion( p, y, n1 , n2)
 
       precision = tp / (tp + fp);
       recall = tp / (tp + fn);
+      
+      % Calculo del F_score para cada clase estudiada
+      F1_Score = 2 * ((precision * recall) / (precision + recall));
+      F1_ScoresMat = [ F1_ScoresMat; F1_Score ];
 
+      % Muestreo de estadisticas
+      
       fprintf('Precision (%d) = %f\n', clase, precision);
       fprintf('Recall (%d) = %f\n', clase, recall);
+      fprintf('F_Score (%d) = %f\n', clase, F1_Score);
   end
 
+  figure;
+  title('Comparativa de clases');
+  xlabel('Clases');
+  ylabel('F1_Score');
+  bar(F1_ScoresMat)
+  legend ('F1_Score','Location','NorthWest')
+  
 end
